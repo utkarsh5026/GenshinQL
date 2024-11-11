0b9152df9cd8b05bb61e6a11f7adca1c5677c1d1
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-11-11 17:14:25 +0530

    added weapons query

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -1,87 +1,102 @@
-import {gql} from "@apollo/client";
+import { gql } from "@apollo/client";
 
 export const GET_CHARACTERS = gql`
-    query GetCharacters {
-        characters {
-            name
-            iconUrl
-            rarity
-            element
-            weaponType
-            region
-            elementUrl
-            weaponUrl
-            regionUrl
-        }
+  query GetCharacters {
+    characters {
+      name
+      iconUrl
+      rarity
+      element
+      weaponType
+      region
+      elementUrl
+      weaponUrl
+      regionUrl
     }
+  }
 `;
 
 export const GET_CHARACTER_FOR_SIDEBAR = gql`
-    query GetCharacterForSidebar {
-        characters {
-            name
-            iconUrl
-        }
+  query GetCharacterForSidebar {
+    characters {
+      name
+      iconUrl
     }
+  }
 `;
 
 export const GET_CHARACTER = gql`
-    query Characters($name: String!) {
-        character(name: $name) {
-            constellations {
-                description
-                iconUrl
-                level
-                name
-            }
-            weaponUrl
-            weaponType
-            talents {
-                talentType
-                talentName
-                talentIcon
-                description
-                figureUrls {
-                    url
-                    caption
-                }
-                scaling {
-                    key
-                    value
-                }
-            }
-            regionUrl
-            region
-            rarity
-            name
-            imageUrls {
-                wish
-                nameCard
-                inGame
-                card
-            }
-            iconUrl
-            elementUrl
-            element
+  query Characters($name: String!) {
+    character(name: $name) {
+      constellations {
+        description
+        iconUrl
+        level
+        name
+      }
+      weaponUrl
+      weaponType
+      talents {
+        talentType
+        talentName
+        talentIcon
+        description
+        figureUrls {
+          url
+          caption
         }
+        scaling {
+          key
+          value
+        }
+      }
+      regionUrl
+      region
+      rarity
+      name
+      imageUrls {
+        wish
+        nameCard
+        inGame
+        card
+      }
+      iconUrl
+      elementUrl
+      element
     }
+  }
 `;
 
-
 export const GET_TALENT_MATERIALS_CALENDAR = gql`
-    query TalentBooks {
-        talentBooks {
-            location
-            days {
-                day
-                books {
-                    name
-                    url
-                }
-                characters {
-                    url
-                    name
-                }
-            }
+  query TalentBooks {
+    talentBooks {
+      location
+      days {
+        day
+        books {
+          name
+          url
         }
-    }`
+        characters {
+          url
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const GET_WEAPONS = gql`
+  query Weapons {
+    weapons {
+      type
+      weapons {
+        type
+        rarity
+        name
+        attack
+        subStat
+      }
+    }
+  }
+`;
--- client/src/graphql/types.d.ts
+++ client/src/graphql/types.d.ts
@@ -1,61 +1,69 @@
 export type Character = {
-    name: string;
-    iconUrl: string;
-    rarity: string;
-    element: string;
-    weaponType: string;
-    region: string;
-    elementUrl: string;
-    weaponUrl: string;
-    regionUrl: string;
+  name: string;
+  iconUrl: string;
+  rarity: string;
+  element: string;
+  weaponType: string;
+  region: string;
+  elementUrl: string;
+  weaponUrl: string;
+  regionUrl: string;
 };
 
 type TalentScale = {
-    key: string;
-    value: string[];
-}
+  key: string;
+  value: string[];
+};
 
 export type Talent = {
-    talentName: string;
-    talentIcon: string;
-    talentType: string;
-    description: string;
-    figureUrls: {
-        url: string;
-        caption: string;
-    }[];
-    scaling: TalentScale[];
+  talentName: string;
+  talentIcon: string;
+  talentType: string;
+  description: string;
+  figureUrls: {
+    url: string;
+    caption: string;
+  }[];
+  scaling: TalentScale[];
 };
 
 export type Constellation = {
-    iconUrl: string;
-    name: string;
-    description: string;
-    level: string;
+  iconUrl: string;
+  name: string;
+  description: string;
+  level: string;
 };
 
 export type CharacterDetailed = Character & {
-    talents: Talent[];
-    constellations: Constellation[];
-    imageUrls: {
-        card: string;
-        wish: string;
-        inGame: string;
-        nameCard: string;
-    };
+  talents: Talent[];
+  constellations: Constellation[];
+  imageUrls: {
+    card: string;
+    wish: string;
+    inGame: string;
+    nameCard: string;
+  };
 };
 
 export type TalentBookCalendar = {
-    location: string;
-    days: {
-        day: string,
-        books: {
-            name: string,
-            url: string
-        }[]
-        characters: {
-            name: string,
-            url: string
-        }[]
+  location: string;
+  days: {
+    day: string;
+    books: {
+      name: string;
+      url: string;
     }[];
-}
+    characters: {
+      name: string;
+      url: string;
+    }[];
+  }[];
+};
+
+export type Weapon = {
+  name: string;
+  iconUrl: string;
+  rarity: string;
+  type: string;
+  subStat: string;
+};
