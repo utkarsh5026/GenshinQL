16649675aedc3e9fb047fcf29db4a8de86a10030
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-11-29 01:17:08 +0530

    added func to get next table sibling

--- server/src/data/utils.ts
+++ server/src/data/utils.ts
@@ -1,3 +1,6 @@
+import { By, WebDriver } from "selenium-webdriver";
+import { waitForElementCss } from "./setup";
+
 export const underScore = (str: string) => str.split(" ").join("_");
 
 export const parseCharacterName = (name: string) => name.split(" ").join("_");
@@ -5,3 +8,20 @@ export const parseCharacterName = (name: string) => name.split(" ").join("_");
 export const toOriginalName = (name: string) => name.split("_").join(" ");
 
 export const parseUrl = (url: string) => url.split("/revision/")[0];
+
+/**
+ * Finds the next table sibling element after the parent of a specified element
+ * @param driver - The Selenium WebDriver instance
+ * @param selector - CSS selector to find the initial element
+ * @returns Promise resolving to the found table element
+ */
+export const getParentNextTableSibling = async (
+  driver: WebDriver,
+  selector: string,
+) => {
+  await waitForElementCss(driver, selector);
+  return driver
+    .findElement(By.css(selector))
+    .findElement(By.xpath(".."))
+    .findElement(By.xpath("following-sibling::table"));
+};
