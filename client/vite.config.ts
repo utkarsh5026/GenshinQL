57e0ccb6b020f2d433b802442533c358626dcccc
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-01-30 01:12:33 +0530

    chore: update package.json and add Vercel configuration
    - Removed the '--base' option from the 'scrape:characters' script in package.json.
    - Added 'react-compiler-runtime', 'babel-plugin-react-compiler', 'chalk', and 'chromedriver' as dependencies in package.json.
    - Created a new vercel.json file for Vercel deployment configuration.
    - Updated vite.config.ts to use 'node:path' and integrated 'babel-plugin-react-compiler' with ReactCompilerConfig.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,23 @@
-import path from "path";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const ReactCompilerConfig = {
+  /* ... */
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "react/compiler-runtime": "react-compiler-runtime",
     },
   },
 });
