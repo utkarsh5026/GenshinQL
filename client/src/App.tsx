7c599a6357e4dd83185d69c569ff081c0ee1b688
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-02-05 08:58:50 +0530

    feat: add ErrorBoundary component for improved error handling in the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,10 @@ import {
   usePrimitivesStore,
   useTalentBooksStore,
   useWeaponMaterialStore,
+  useWeaponsStore,
 } from '@/stores';
 
+import ErrorBoundary from './components/ErrorBoundary';
 import { Layout } from './components/layout';
 import { routes } from './routes';
 
@@ -15,16 +17,26 @@ function App() {
   const { fetchPrimitives } = usePrimitivesStore();
   const { fetchWeaponMaterials } = useWeaponMaterialStore();
   const { fetchBooks } = useTalentBooksStore();
+  const { fetchWeapons } = useWeaponsStore();
 
   useAutoClearOldCache(7);
 
   useEffect(() => {
-    Promise.all([fetchPrimitives(), fetchWeaponMaterials(), fetchBooks()]);
-  }, [fetchPrimitives, fetchWeaponMaterials, fetchBooks]);
+    const loadData = async () => {
+      await Promise.all([fetchPrimitives(), fetchWeapons(), fetchBooks()]);
+      await fetchWeaponMaterials();
+    };
+
+    loadData();
+  }, [fetchPrimitives, fetchWeapons, fetchWeaponMaterials, fetchBooks]);
 
   const routing = useRoutes(routes);
 
-  return <Layout>{routing}</Layout>;
+  return (
+    <ErrorBoundary>
+      <Layout>{routing}</Layout>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
