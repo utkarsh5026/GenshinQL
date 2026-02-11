fcd02014f289f7faf6f7284b4a6437f2725d2883
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-02-11 11:11:44 +0530

    refactor(weapons): improve readability of nation and weekday assignments in fetchWeaponsFile

--- client/src/features/weapons/services/fetchWeapons.ts
+++ client/src/features/weapons/services/fetchWeapons.ts
@@ -18,12 +18,19 @@ export async function fetchWeaponsFile(): Promise<Array<WeaponSummary>> {
       ...weaponList.map((w) => {
         return {
           ...w,
-          nation: w.nation && w.nation !== -1 ? nations[w.nation] : 'N/A',
-          weekday: w.weekdays && w.weekdays !== -1 ? days[w.weekdays] : 'N/A',
+          nation:
+            w.nation !== undefined && w.nation !== -1
+              ? nations[w.nation]
+              : 'N/A',
+          weekday:
+            w.weekdays !== undefined && w.weekdays !== -1
+              ? days[w.weekdays]
+              : 'N/A',
           weaponType: wepType,
         };
       })
     );
   }
+
   return finalWeapons;
 }
