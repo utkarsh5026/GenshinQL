4bae413fbd92bd761b3963ae671790904f7e6da9
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-02-14 04:10:44 +0530

    feat: add AppHeader component to layout for improved mobile navigation

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { GlobalSearch } from '@/features/command-palette';
 
+import { AppHeader } from './header';
 import { Sidebar } from './sidebar';
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
@@ -15,6 +16,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
       <GlobalSearch />
 
       <div className="flex flex-col flex-1 md:ml-16">
+        <AppHeader onMenuClick={() => setMobileMenuOpen(true)} />
         <main className="flex-1 overflow-auto p-4 md:p-6 m-4">{children}</main>
       </div>
     </div>
